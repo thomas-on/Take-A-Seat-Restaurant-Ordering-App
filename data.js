a3971434a0061752046064169d7d413a2ea7f1b7
Thomas On <thomas_on@hotmail.com>
2023-08-04 17:12:45 +0800

    Initial commit

---
+++ data.js
@@ -0,0 +1,32 @@
+
+
+export const menuArray = [
+    {
+        name: "Chicken Satay",
+        ingredients: ["chicken on a skewer", " peanut sauce", " onions", " cucumber"],
+        id: '0',
+        price: 14,
+        itemPic: "images/chicken-satay.png"
+    },
+    {
+        name: "Fish Noodle Soup",
+        ingredients: ["fish slices", " noodle", " lettuce", " bittergourd", " tomatoes"],
+        id: '1',
+        price: 16,
+        itemPic: "images/sliced-fish-noodle-soup.png"
+    },
+        {
+        name: "Masala Dosa",
+        ingredients: ["potatoes", " rice", " ural dal", " chana dal", " ginger", " onions"],
+        id: '2',
+        price: 15,
+        itemPic: "images/masala-dosa.png"
+    },
+        {
+        name: "Grilled Sambal Stingray",
+        ingredients: ["stingray", " peanut sauce", " dried shrimp sambal", " banana leaf", ' lime juice'],
+        id: '3',
+        price: 16,
+        itemPic: "images/grilled-sambal-stingray.png"
+    }
+]
